a415878cb65cffb97b5ad69e39839298482422e9
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-07-08 16:56:28 +0600

    added features section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
-import CarouselContainer from "./Carousel";
-import Catagory from "./Catagory";
+import Boss from '../../Components/Boss/Boss';
+import Menu from '../Shared/Menu/Menu';
+import CarouselContainer from './Carousel';
+import Catagory from './Catagory';
+import Fetures from './Fetures';
 
 const Home = () => {
   return (
     <div>
-      <CarouselContainer/>
-      <Catagory/>
+      <CarouselContainer />
+      <Catagory />
+      <Boss/> 
+      <Menu />
+      <Fetures/>
     </div>
   );
 };
 
 export default Home;
- 
