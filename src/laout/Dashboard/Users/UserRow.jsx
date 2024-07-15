c8f5f5fa2a43c92d2654eb401e69f13dda1b08f8
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-07-15 06:26:38 +0600

    added users delete functionality

--- src/laout/Dashboard/Users/UserRow.jsx
+++ src/laout/Dashboard/Users/UserRow.jsx
@@ -1,12 +1,47 @@
 import { BsPersonLinesFill } from "react-icons/bs";
 import { MdDelete } from "react-icons/md";
+import { toast } from "react-hot-toast";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import useUsers from "../../../Hooks/useUsers";
 
 const UserRow = ({ user, index }) => {
-  const { email, imageUrl, userName } = user;
+  const { email, imageUrl, userName, _id } = user;
+  const axiosSecure = useAxiosSecure();
+  const [, refetch] = useUsers();
+
+  const handleDelete = async () => {
+    //  make a asking before delete with Sweetalert2
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    });
+
+    if (result.isConfirmed) {
+      try {
+        const { data } = await axiosSecure.delete(`/users/${_id}`);
+        // make a confrirmations afiter data.dleecteCount > 0
+
+        if (data.deletedCount > 0) {
+          // show  success message with sweeetalert message
+          toast.success("User deleted successfully");
+          refetch();
+        }
+      } catch (error) {
+        toast.error("Failed to delete user");
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <tr>
-      <th> {index+1} </th>
+      <th> {index + 1} </th>
       <td>
         <div className="flex items-center gap-3">
           <div className="avatar">
@@ -25,7 +60,7 @@ const UserRow = ({ user, index }) => {
         <BsPersonLinesFill />
       </td>
       <th>
-        <button className="btn btn-ghost btn-lg">
+        <button onClick={handleDelete} className="btn btn-ghost btn-lg">
           {" "}
           <MdDelete className="text-red-700" />
         </button>
